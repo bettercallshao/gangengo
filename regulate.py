6e4b0a92bf8a0652ef7e9164d29c9521326ce805
Shaoqing Tan <tim.tan@hydiant.com>
2019-03-16 17:25:20 -0400

    Loose up

--- regulate.py
+++ regulate.py
@@ -41,6 +41,8 @@ def df_from_kifu_zip_dir():
     # list files and remove zip suffix
     names = map(lambda x: x[:-4], os.listdir(zip_dir))
 
+    big = None
+
     # loop thru
     for idx, name in enumerate(names):
         if idx > 10:
@@ -52,12 +54,38 @@ def df_from_kifu_zip_dir():
         kifu = zf.read(name).decode('utf-8')
         # convert to pandas
         df = df_from_kifu(kifu)
-        df.to_csv(os.path.join(out_dir, name + '.csv'), index=None)
+        if big is None:
+            big = df
+        else:
+            big = pd.concat([big, df])
+
+    return big
 
 
-def viz():
-    df = pd.read_csv(os.path.join(out_dir, '2016-07.csv'))
-    row = df.iloc[90]
+def df_from_noise():
+    N_FAKE = 140*1000
+    arr = np.random.randint(0, 4, size=(N_FAKE, S*S)) / 3
+    arr[(arr > 0) & (arr < 1)] = 0.5
+    df = pd.DataFrame(arr)
+    return df
+
+
+def make_csv():
+    neg = df_from_noise()
+    neg['go'] = 0
+    pos = df_from_kifu_zip_dir()
+    pos['go'] = 1
+
+    big = pd.concat([pos, neg])
+    big.to_csv(os.path.join(out_dir, 'go.csv'), index=None)
+
+
+def viz(t):
+    if t == 'pos':
+        df = pd.read_csv(os.path.join(out_dir, '2016-07.csv'))
+    else:
+        df = df_from_noise()
+    row = df.iloc[np.random.randint(0, df.shape[0])]
 
     plt.figure()
     loc = MultipleLocator(base=1)
@@ -70,4 +98,17 @@ def viz():
             plt.scatter(idx % S, int(idx / 19), c='black', s=50)
         elif row[idx] < 0.25:
             plt.scatter(idx % S, int(idx / 19), c='red', s=50)
+
+
+def show():
     plt.show()
+
+
+def compare():
+    while True:
+        viz('pos')
+        viz('neg')
+        show()
+
+
+compare()
